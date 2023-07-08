9447ce9f29d0e089754d3d74ee7d43655ea9b8d9
Pilmo Jeong <itsmo1031@gmail.com>
2023-07-08 13:46:59 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 122804 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+A = input()
+M = len(A)
+B = input()
+N = len(B)
+
+lcs = [[0] * (M + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        if B[i - 1] == A[j - 1]:
+            lcs[i][j] = lcs[i - 1][j - 1] + 1
+        else:
+            lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
+
+print(max(lcs[-1]))
