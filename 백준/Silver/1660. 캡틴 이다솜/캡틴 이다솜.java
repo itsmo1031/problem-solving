68ae10e0a2479b56459c6a21add44671d5c53653
Pilmo Jeong <itsmo1031@gmail.com>
2023-07-28 22:16:33 +0900

    [Silver I] Title: 캡틴 이다솜, Time: 172 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/1660. 캡틴 이다솜/캡틴 이다솜.java
+++ 백준/Silver/1660. 캡틴 이다솜/캡틴 이다솜.java
@@ -12,12 +12,14 @@ public class Main {
 
         int N = sc.nextInt();
         int[] cannon = new int[N + 1];
-        for (int i = 1; tetra[i] <= N; i++) {
-            for (int j = tetra[i]; j <= N; j++) {
+        for (int i : tetra) {
+            if (i > N)
+                break;
+            for (int j = i; j <= N; j++) {
                 if (i == 1)
                     cannon[j] = cannon[j - 1] + 1;
                 else
-                    cannon[j] = Math.min(cannon[j - tetra[i]] + 1, cannon[j]);
+                    cannon[j] = Math.min(cannon[j - i] + 1, cannon[j]);
             }
         }
         System.out.println(cannon[N]);
