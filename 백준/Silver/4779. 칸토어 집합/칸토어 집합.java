05655b91af418875a9f1e365c4b01a93587cb5a1
Pilmo Jeong <itsmo1031@gmail.com>
2023-08-15 09:14:06 +0900

    [Silver III] Title: 칸토어 집합, Time: 264 ms, Memory: 23200 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    static String cantor(int n, String str) {
-        if (n == 0)
-            return "-";
-        String newStr = cantor(n - 1, "-".repeat((int) Math.pow(3, n - 1)));
-        return newStr + " ".repeat((int) Math.pow(3, n - 1)) + newStr;
+    static StringBuilder sb;
+
+    static void cantor(int start, int length) {
+        if (length == 1)
+            return;
+
+        int newLength = length / 3;
+
+        for (int idx = start + newLength; idx < start + 2 * newLength; idx++) {
+            sb.setCharAt(idx, ' ');
+        }
+
+        cantor(start, newLength);
+        cantor(start + 2 * newLength, newLength);
     }
 
     public static void main(String[] args) {
@@ -13,8 +22,15 @@ public class Main {
 
         while (sc.hasNext()) {
             int N = sc.nextInt();
-            String str = cantor(N, "-".repeat((int) Math.pow(3, N)));
-            System.out.println(str);
+            int length = (int) Math.pow(3, N);
+            sb = new StringBuilder();
+
+            for (int idx = 0; idx < length; idx++) {
+                sb.append("-");
+            }
+
+            cantor(0, length);
+            System.out.println(sb);
         }
 
         sc.close();
