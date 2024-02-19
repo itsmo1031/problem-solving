fea9e519775782b7eb109e24aa0ef5f52d74dd36
Pilmo Jeong <itsmo1031@gmail.com>
2024-02-19 14:25:30 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> yearningMap = new HashMap<>();
+    for (int i = 0; i < name.length; i++) {
+      yearningMap.put(name[i], yearning[i]);
+    }
+
+    int[] answer = new int[photo.length];
+
+    for (int i = 0; i < photo.length; i++) {
+      for (int j = 0; j < photo[i].length; j++) {
+        if (yearningMap.containsKey(photo[i][j])) {
+          answer[i] += yearningMap.get(photo[i][j]);
+        }
+      }
+    }
+
+    return answer;
+    }
+}
