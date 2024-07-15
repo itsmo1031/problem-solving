964170f61241a1b9bb2ed0f8d0797dc42fca0b65
Pilmo Jeong <itsmo1031@gmail.com>
2024-07-15 15:06:30 +0900

    [level 2] Title: [3차] n진수 게임, Time: 14.81 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        int maxLength = t * m;
+        StringBuilder sb = new StringBuilder();
+        int number = 0;
+        
+        while (sb.length() < maxLength) {
+            sb.append(Integer.toString(number++, n).toUpperCase());
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = p - 1; i < t * m; i += m) {
+            answer.append(sb.charAt(i));
+        }
+        
+        return answer.toString();
+    }
+}
