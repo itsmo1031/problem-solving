1558a5e0c985bf8738b3b145c0a8dc3f37c9da93
Pilmo Jeong <itsmo1031@gmail.com>
2024-07-23 20:33:24 +0900

    [level 3] Title: 외벽 점검, Time: 70.90 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] weak, int[] dist) {
-        int[] weakPoint = Arrays.copyOf(weak, weak.length * 2);
-        for (int i = weak.length; i < weakPoint.length; i++) {
-            weakPoint[i] = weak[i - weak.length] + n;
+        int[] newWeak = Arrays.copyOf(weak, weak.length * 2);
+        for (int i = weak.length; i < newWeak.length; i++) {
+            newWeak[i] = weak[i - weak.length] + n;
         }
         
+        Arrays.sort(dist);
         
         int answer = 9;
         
         do {
             for (int start = 0; start < weak.length; start++) {
                 int worker = 1;
-                int current = weakPoint[start] + dist[worker - 1];
-
-                for (int idx = start; idx < start + weak.length; idx++) {
-                    if (current >= weakPoint[idx]) {
+                int current = newWeak[start] + dist[worker - 1];
+                
+                for (int idx = start + 1; idx < start + weak.length; idx++) {
+                    if (current >= newWeak[idx]) {
                         continue;
                     }
-                    if (++worker > dist.length) {
+                    worker += 1;
+                    if (worker > dist.length) {
                         break;
                     }
-                    current = weakPoint[idx] + dist[worker - 1];
+                    current = newWeak[idx] + dist[worker - 1];
                 }
                 answer = Math.min(answer, worker);
-                if (answer == 1) {
-                    return answer;
-                }
             }
-        } while (nextPermutation(dist));
+        } while(nextPermutation(dist));
         
         if (answer > dist.length) {
             answer = -1;
@@ -37,8 +36,9 @@ class Solution {
         
         return answer;
     }
-        
-    static boolean nextPermutation(int[] arr) {
+    
+    // next permutation
+    boolean nextPermutation(int[] arr) {
         int top = arr.length - 1;
         
         while (top > 0 && arr[top - 1] >= arr[top]) {
@@ -50,10 +50,12 @@ class Solution {
         }
         
         int target = arr.length - 1;
-        while (arr[target] <= arr[top - 1]) {
+        
+        while (arr[top - 1] >= arr[target]) {
             target--;
         }
         
+        // switch
         int temp = arr[top - 1];
         arr[top - 1] = arr[target];
         arr[target] = temp;
